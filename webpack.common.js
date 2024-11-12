c9c5743ff4946e7071df48c47eebe1974d5768af
akhanamiryan <ashot.khanamiryan@volo.global>
2024-11-12 13:11:16 +0400

    fix: add hash to url

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,7 @@ const plugins = [
     template: './src/index.html',
     chunks: ['main'],
     inject: 'body',
+    hash: true,
   }),
   new webpack.DefinePlugin({
     'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID).trim(),
